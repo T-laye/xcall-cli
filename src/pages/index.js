61aa2f7dec29728abdbd676c082c91525f520ec9
Laye <tlayefaah@gmail.com>
2023-11-30 17:17:50 +0100

    debugging

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <Layout>
-      <section className="container mx-auto pt-14 lg:pt-24 min-h-[100vh] lg:min-h-[100vh] 2xl:min-h-[100vh] flex items-center lg:items-start lg:justify-center justify-start flex-col lg:flex-row ">
+      <section className="container mx-auto pt-14 lg:pt-24 h-[100vh] lg:h-[100vh] 2xl:h-[100vh] flex items-center lg:items-start lg:justify-center justify-start flex-col lg:flex-row ">
         <div className="lg:w-1/2 pt-24 flex justify-cente flex-col mb-10 lg:mb-0">
           <div className="flex flex-col items-center lg:items-start ">
             <h1 className="text-7xl font-bold text-primary  ">Validator</h1>
@@ -16,7 +16,7 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="lg:w-3/4 mt-5">
+            <div className="lg:w-3/4 w-full mt-5">
               <input type="text" />
             </div>
           </div>
@@ -27,15 +27,15 @@ export default function Home() {
             </button>
           </div>
         </div>
-        <div className="lg:w-1/2 bg-green-30 h-full flex relative items-center justify-center overflow-hiden object-cover ">
+        <div className="lg:w-1/2 bg-green-30 h-full flex relative items-center justify-center overflow-hiden object-cover opacity-80">
           <Image
             src="https://cdn.dribbble.com/users/1373613/screenshots/5510801/media/adf3f3a5acf468eb1a2e23849ddbe4ec.gif"
             alt="validation"
             height={1000}
             width={1000}
-            className="object-cover w-full h-full lg:mb-20"
+            className="object-contain w-full h-full lg:mb-20"
           />
-          <div className="absolute top-0 left-0 right-0 bottom-0 bg-primary mix-blend-darken ">
+          <div className="absolute top-0 left-0 right-0 bottom-0 b mix-blend-darken ">
             {" "}
           </div>
         </div>
